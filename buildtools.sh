f62c23db188e65f6552baf6dfbcc418734810e5b
Giacomo Tesio <giacomo@tesio.it>
2015-12-23 00:29:52 +0100

    buildtools.sh: inherit $UTILITIES if defined

--- buildtools.sh
+++ buildtools.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cd `dirname $0`
-UTILITIES=`pwd`
+if [ -z "$UTILITIES" ]; then
+	UTILITIES=`pwd`
+fi
 git clean -x -d -f $UTILITIES
 echo -n Building development tools...
 GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go get -d jehanne/cmd/...
