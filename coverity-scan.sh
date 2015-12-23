0d8e5b4df98c7d41f2961b26a3d0fd8c9b7dfe14
Giacomo Tesio <giacomo@tesio.it>
2015-12-23 01:00:36 +0100

    coverity-scan.sh: inherity $JEHANNE if defined

--- coverity-scan.sh
+++ coverity-scan.sh
@@ -2,8 +2,10 @@
 set -e
 
 if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then
-	export JEHANNE=`git rev-parse --show-toplevel|sed 's/\/hacking//g'`
-	export PATH="$JEHANNE/hacking:$PATH"
+	if [ -z "$JEHANNE" ]; then
+		export JEHANNE=`git rev-parse --show-toplevel|sed 's/\/hacking//g'`
+		export PATH="$JEHANNE/hacking/bin:$PATH"
+	fi
 	export SH=`which rc`
 	export ARCH=amd64
 	git clean -x -d -f
