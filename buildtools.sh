f01be05b102abfe73429a07ca75ea256fa6c889c
Giacomo Tesio <giacomo@tesio.it>
2016-01-07 16:39:55 +0100

    buildtools.sh: build drawterm (printing output only on fail)

--- buildtools.sh
+++ buildtools.sh
@@ -5,8 +5,32 @@ if [ -z "$UTILITIES" ]; then
 fi
 git clean -x -d -f $UTILITIES
 echo -n Building development tools...
-GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go get -d jehanne/cmd/...
-GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go install jehanne/cmd/...
-GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go install github.com/lionkov/ninep/srv/examples/ufs
+(
+	GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go get -d jehanne/cmd/... &&
+	GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go install jehanne/cmd/... &&
+	GOBIN="$UTILITIES/bin" GOPATH="$UTILITIES/third_party:$UTILITIES" go install github.com/lionkov/ninep/srv/examples/ufs
+)
+STATUS="$?"
+if [ ! $STATUS -eq "0" ]
+then
+        echo "FAIL"
+	exit $STATUS
+fi
 
-echo " DONE."
+(
+	cd $UTILITIES/third_party/src/github.com/0intro/drawterm/ && 
+	git clean -xdf > ../drawterm.build.log 2>&1 &&
+	CONF=unix make > ../drawterm.build.log 2>&1 &&
+	mv drawterm $UTILITIES/bin
+)
+STATUS="$?"
+if [ $STATUS -eq "0" ]
+then
+	rm $UTILITIES/third_party/src/github.com/0intro/drawterm.build.log
+	echo "DONE"
+else
+	echo "FAIL"
+	cat $UTILITIES/third_party/src/github.com/0intro/drawterm.build.log
+fi
+
+exit $STATUS
