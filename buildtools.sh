0ca3a09cb96c51565d6bbff1a9c73608c1d1d3b5
Giacomo Tesio <giacomo@tesio.it>
2016-02-01 17:26:12 +0000

    buildtools.sh: build ken-cc

--- buildtools.sh
+++ buildtools.sh
@@ -17,20 +17,40 @@ then
 	exit $STATUS
 fi
 
+# Drawterm
 (
 	cd $UTILITIES/third_party/src/github.com/0intro/drawterm/ && 
 	git clean -xdf > ../drawterm.build.log 2>&1 &&
-	CONF=unix make > ../drawterm.build.log 2>&1 &&
+	CONF=unix make >> ../drawterm.build.log 2>&1 &&
 	mv drawterm $UTILITIES/bin
 )
 STATUS="$?"
 if [ $STATUS -eq "0" ]
 then
 	rm $UTILITIES/third_party/src/github.com/0intro/drawterm.build.log
-	echo "DONE"
 else
 	echo "FAIL"
 	cat $UTILITIES/third_party/src/github.com/0intro/drawterm.build.log
+	exit $STATUS
+fi
+
+# Plan9 compilers
+(
+        cd $UTILITIES/third_party/src/github.com/JehanneOS/devtools-kencc && 
+	./configure > ../kencc.build.log 2>&1 &&
+	. ./env &&
+	export >> ../kencc.build.log 2>&1 &&
+	mk >> ../kencc.build.log 2>&1 &&
+	mk install 2>&1 >> ../kencc.build.log
+)
+STATUS="$?"
+if [ $STATUS -eq "0" ]
+then
+        rm $UTILITIES/third_party/src/github.com/JehanneOS/kencc.build.log
+        echo "DONE"
+else
+        echo "FAIL"
+        cat $UTILITIES/third_party/src/github.com/JehanneOS/kencc.build.log
 fi
 
 exit $STATUS
