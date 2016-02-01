5d1a8c50526b35bc7c584bf8a3657c35871300e8
Giacomo Tesio <giacomo@tesio.it>
2016-02-01 17:26:43 +0000

    devshell.sh: add ken-cc to PATH

--- devshell.sh
+++ devshell.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export JEHANNE=`git rev-parse --show-toplevel`
 export PATH="$JEHANNE/hacking/bin:$PATH"
+export PATH="$JEHANNE/hacking/third_party/src/github.com/JehanneOS/devtools-kencc/bin:$PATH"
 export ARCH=amd64
 
 bash --rcfile <(cat ~/.bashrc; echo 'PS1="JehanneDEV $PS1"')
