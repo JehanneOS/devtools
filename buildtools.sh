42fc110d67c981324100abee589fbcc3d9452916
Giacomo Tesio <giacomo@tesio.it>
2016-04-14 23:54:30 +0200

    buildtools: kencc is not needed anymore

--- buildtools.sh
+++ buildtools.sh
@@ -34,23 +34,4 @@ else
 	exit $STATUS
 fi
 
-# Plan9 compilers
-(
-        cd $UTILITIES/third_party/src/github.com/JehanneOS/devtools-kencc && 
-	./configure > ../kencc.build.log 2>&1 &&
-	. ./env &&
-	export >> ../kencc.build.log 2>&1 &&
-	mk >> ../kencc.build.log 2>&1 &&
-	mk install 2>&1 >> ../kencc.build.log
-)
-STATUS="$?"
-if [ $STATUS -eq "0" ]
-then
-        rm $UTILITIES/third_party/src/github.com/JehanneOS/kencc.build.log
-        echo "DONE"
-else
-        echo "FAIL"
-        cat $UTILITIES/third_party/src/github.com/JehanneOS/kencc.build.log
-fi
-
 exit $STATUS
