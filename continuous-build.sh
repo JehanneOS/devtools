76025ed4b0a2ddd968c5b52c2713d3bf3e801cf1
Giacomo Tesio <giacomo@tesio.it>
2016-11-26 17:55:21 +0100

    travis: fix paths for caches

--- continuous-build.sh
+++ continuous-build.sh
@@ -17,7 +17,7 @@ if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then
 		echo "Cannot find cross-compiling toolchain in $JEHANNE/hacking/bin"
 		if [ -f "$JEHANNE/tmp/toolchain/bin/x86_64-jehanne-gcc" ]; then
 			echo "Found cross-compiling toolchain in $JEHANNE/tmp/toolchain/"
-			mv $JEHANNE/tmp/toolchain/ $JEHANNE/hacking/cross/toolchain
+			mv $JEHANNE/tmp/toolchain/* $JEHANNE/hacking/cross/toolchain/
 		else
 			echo "Creating cross-compiling toolchain..."
 			(cd $JEHANNE/hacking/cross/; ./init.sh)
@@ -40,6 +40,9 @@ if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then
 
 	if [ "$TRAVIS_BUILD_DIR" != "" ]; then
 		echo "Move cross-compiling toolchain to $JEHANNE/tmp/toolchain for Travis caches"
-		mv $JEHANNE/hacking/cross/toolchain $JEHANNE/tmp/toolchain/
+		if [ ! -d "$JEHANNE/tmp/toolchain" ]; then
+			mkdir $JEHANNE/tmp/toolchain
+		fi
+		mv $JEHANNE/hacking/cross/toolchain/* $JEHANNE/tmp/toolchain/
 	fi
 fi
--- coverity-scan.sh
+++ coverity-scan.sh
@@ -16,7 +16,7 @@ if [ ! -f "$JEHANNE/hacking/cross/toolchain/bin/x86_64-jehanne-gcc" ]; then
 	echo "Cannot find cross-compiling toolchain in $JEHANNE/hacking/bin"
 	if [ -f "$JEHANNE/tmp/toolchain/bin/x86_64-jehanne-gcc" ]; then
 		echo "Found cross-compiling toolchain in $JEHANNE/tmp/toolchain/"
-		mv $JEHANNE/tmp/toolchain/ $JEHANNE/hacking/cross/toolchain
+		mv $JEHANNE/tmp/toolchain/* $JEHANNE/hacking/cross/toolchain/
 	else
 		echo "Creating cross-compiling toolchain..."
 		(cd $JEHANNE/hacking/cross/; ./init.sh)
@@ -32,5 +32,8 @@ build all
 
 if [ "$TRAVIS_BUILD_DIR" != "" ]; then
 	echo "Move cross-compiling toolchain to $JEHANNE/tmp/toolchain for Travis caches"
-	mv $JEHANNE/hacking/cross/toolchain $JEHANNE/tmp/toolchain/
+	if [ ! -d "$JEHANNE/tmp/toolchain" ]; then
+		mkdir $JEHANNE/tmp/toolchain
+	fi
+	mv $JEHANNE/hacking/cross/toolchain/* $JEHANNE/tmp/toolchain/
 fi
