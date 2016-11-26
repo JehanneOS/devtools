b77038020caaa2e3cd6f3aada081fd1b7b76580f
Giacomo Tesio <giacomo@tesio.it>
2016-11-27 00:04:48 +0100

    coverity-scan.sh: remove cov-configure from prepare

--- coverity-scan.sh
+++ coverity-scan.sh
@@ -30,8 +30,6 @@ if [ "$1" != "prepare" ]; then
 	export TOOLPREFIX=x86_64-jehanne-
 	export CC=x86_64-jehanne-gcc
 	build all
-else
-	cov-configure --gcc --comptype gcc --compiler $JEHANNE/hacking/cross/toolchain/bin/x86_64-jehanne-gcc
 fi
 
 if [ "$TRAVIS_BUILD_DIR" != "" ]; then
