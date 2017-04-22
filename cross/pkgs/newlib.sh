0f5de042070bcfb2ba8d380c03699efb40e1bd9a
Giacomo Tesio <giacomo@tesio.it>
2017-04-22 18:04:56 +0200

    newlib: travis-ci madness... why?

--- cross/pkgs/newlib.sh
+++ cross/pkgs/newlib.sh
@@ -50,6 +50,15 @@ if [ "$NEWLIB_OPTIMIZATION" = "" ]; then
 	NEWLIB_OPTIMIZATION=2
 fi
 
+if [ "$TRAVIS_BUILD_DIR" != "" ]; then
+	# something weird happens on Travis-CI so that newlib requires
+	# explicit set of the HOST compiler or it will use the
+	# Jehanne cross compiler and try to run the executable on linux
+	#
+	# I can't understand why...
+	export CC=gcc
+fi
+export CC=gcc
 # NOTE: we use -O0 because apparently vsprintf functions do not
 #       work with -O2.
 export CFLAGS_FOR_TARGET="-g -gdwarf-2 -ggdb -O$NEWLIB_OPTIMIZATION"
