8d419405cbb13912c301f45f928d2d5b3dc86a1e
Giacomo Tesio <giacomo@tesio.it>
2020-09-01 13:10:12 +0200

    gcc native: remove cross/native.sh and unused patches in cross/pkgs/gcc/patch
    Apparently we can use the patches created for the cross compiler.
    
    cross/native.sh has been moved in cross/pkgs/gcc/build.sh to get some
    coherence among the packages (in the hope of writing a reasonable
    package manager in the future).

--- cross/pkgs/gcc/patch/binutils/ld/emulparams/elf_i386_jehanne.sh
+++
@@ -1,4 +0,0 @@
-. ${srcdir}/emulparams/elf_i386.sh
-GENERATE_SHLIB_SCRIPT=
-GENERATE_PIE_SCRIPT=yes
-ENTRY=_main
--- cross/pkgs/gcc/patch/binutils/ld/emulparams/elf_x86_64_jehanne.sh
+++
@@ -1,4 +0,0 @@
-. ${srcdir}/emulparams/elf_x86_64.sh
-GENERATE_SHLIB_SCRIPT=
-GENERATE_PIE_SCRIPT=yes
-ENTRY=_main
